32bae4f4d5d371065c9585f55a07e95b03ddcbf4
JordanoBaluz <jordanolb95@hotmail.com>
2022-12-12 21:52:51 -0300

    passado alguns conceitos teoricos, configurado o prisma

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,3 +8,15 @@ import { AppService } from './app.service';
   providers: [AppService],
 })
 export class AppModule {}
+
+/* 
+Decorator - acompla um funcionamento dentro de outra váriavel, classe, função, etc
+exemplo de decorator @
+Controllers - arquivos que são pontos de entrada na aplicação, por exemplo que lidam com chamadas de html, irá controlar as rotas
+Services - define uma classe e funcionalidades, tem um conceito mais genérico
+Inversão de Dependência - classe que recebe como parametro quando é instânciado ao invés de buscar a depêndencia por importação
+Injeção de Dependência - Automatiza a inserção das dependências quando as classes são instânciadas
+NestJS utiliza inversão e injeção, por ajudar com os testes
+Abstract Class - 
+Prisma - é um ORM
+*/
