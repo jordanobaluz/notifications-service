348cd51c1830f047d5faed5f35fdf30e145dc48e
JordanoBaluz <jordanolb95@hotmail.com>
2022-12-19 21:02:31 -0300

    criado um mapper e feito a chamado durante a passagem dos dados para o banco

--- src/infra/database/prisma/repositories/prisma-notifications-repository.ts
+++ src/infra/database/prisma/repositories/prisma-notifications-repository.ts
@@ -2,22 +2,18 @@ import { Injectable } from '@nestjs/common';
 import { Notification } from '@application/entities/notification';
 import { NotificationsRepository } from '@application/repositories/notifications-repository';
 import { PrismaService } from '../prisma.service';
+import { PrismaNotificationMapper } from '../mappers/prisma-notification-mapper';
 
 @Injectable()
 export class PrismaNotificationsRepository implements NotificationsRepository {
   //faz inversão de dependencia para usar service do prisma
   //apenas repositiorios da camada do prisma tem acesso ao banco
   constructor(private prismaService: PrismaService) {}
+
   async create(notification: Notification): Promise<void> {
+    const raw = PrismaNotificationMapper.toPrisma(notification);
     await this.prismaService.notification.create({
-      data: {
-        id: notification.id,
-        category: notification.category,
-        content: notification.content.value,
-        recipientId: notification.recipientId,
-        readAt: notification.readAt,
-        createdAt: notification.createdAt,
-      },
+      data: raw,
     });
   }
 }
