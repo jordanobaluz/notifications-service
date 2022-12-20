3d0156b33e49e64f0255483e76c65671b3a9a938
JordanoBaluz <jordanolb95@hotmail.com>
2022-12-19 21:52:23 -0300

    criado feature de cancelamento de notificação e

--- src/infra/database/prisma/repositories/prisma-notifications-repository.ts
+++ src/infra/database/prisma/repositories/prisma-notifications-repository.ts
@@ -10,10 +10,18 @@ export class PrismaNotificationsRepository implements NotificationsRepository {
   //apenas repositiorios da camada do prisma tem acesso ao banco
   constructor(private prismaService: PrismaService) {}
 
+  async findById(notificationId: string): Promise<Notification | null> {
+    throw new Error('Method not implemented.');
+  }
+
   async create(notification: Notification): Promise<void> {
     const raw = PrismaNotificationMapper.toPrisma(notification);
     await this.prismaService.notification.create({
       data: raw,
     });
+  }
+
+  async save(notification: Notification): Promise<void> {
+    throw new Error('Method not implemented.');
   }
 }
