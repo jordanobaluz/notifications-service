d5e061a643bd6213a4b25833f13cb23e971d4417
JordanoBaluz <jordanolb95@hotmail.com>
2022-12-19 21:52:17 -0300

    criado testagem dos cancelamentos de notificação

---
+++ src/application/use-cases/cancel-notification.spec.ts
@@ -0,0 +1,42 @@
+import { Content } from '@application/entities/content';
+import { Notification } from '@application/entities/notification';
+import { InMemoryNotificationsRepository } from '@test/repositories/in-memory-notifications-repository';
+import { CancelNotification } from './cancel-notification';
+import { NotificationNotFound } from './errors/notification-not-found';
+
+describe('Cancel notification', () => {
+  it('should be able to Cancel a notification', async () => {
+    const notificationsRepository = new InMemoryNotificationsRepository();
+    const cancelNotification = new CancelNotification(notificationsRepository);
+
+    // cria uma notificação
+    const notification = new Notification({
+      category: 'social',
+      content: new Content('Nova solicitação de amizade'),
+      recipientId: 'example-recipient-id',
+    });
+
+    // adiciona ao banco de dados
+    await notificationsRepository.create(notification);
+    // pra executar precisa de uma notificação já existente e testa se pode cancelar
+    await cancelNotification.execute({
+      notificationId: notification.id,
+    });
+
+    // espera que cancelAt seja igual a qualquer informação do tipo Date
+    expect(notificationsRepository.notifications[0].canceledAt).toEqual(
+      expect.any(Date),
+    );
+  });
+
+  it('should not be able to cancel a non existing notification', async () => {
+    const notificationsRepository = new InMemoryNotificationsRepository();
+    const cancelNotification = new CancelNotification(notificationsRepository);
+
+    expect(() => {
+      return cancelNotification.execute({
+        notificationId: 'fake-notification-id',
+      });
+    }).rejects.toThrow(NotificationNotFound);
+  });
+});
